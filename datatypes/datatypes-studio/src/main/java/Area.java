c1df2fed5dd465822bcdaaa1025de8e284eb8a0b
jayheid <jp.heidbrink@gmail.com>
2024-09-05 20:06:13 -0500

    Modified studio. Added bonus.

--- datatypes/datatypes-studio/src/main/java/Area.java
+++ datatypes/datatypes-studio/src/main/java/Area.java
@@ -14,8 +14,23 @@ public class Area {
         System.out.println("Enter a radius: ");
         radius = input.nextDouble();
         // area = Math.PI * Math.pow(radius,2);
+        while (radius < 0) {
+            System.out.println("Enter a VALID radius: ");
+            radius = input.nextDouble();
+        }
+
         area = Circle.getArea(radius);
         System.out.println("The area of a circle of radius " + radius + " is: " + area + "cm\u00B2");
+
+        /*
+        if (radius > 0){
+            area = Circle.getArea(radius);
+            System.out.println("The area of a circle of radius " + radius + " is: " + area + "cm\u00B2");
+        }
+        else{
+            System.out.println("Invalid input!");
+        }
+        */
 
         input.close();
     }
