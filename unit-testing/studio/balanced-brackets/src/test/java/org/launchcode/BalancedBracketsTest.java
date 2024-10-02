5919c32775300bc0223dc5a7ad8d6f8b798146bb
jayheid <jp.heidbrink@gmail.com>
2024-10-02 16:18:47 -0500

    Modified studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,61 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void onlyClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void ReturnsTrueWithOtherChar () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets( "[word]"));
+    }
+    @Test
+    public void RetunrsFalseWithOtherChar () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]word["));
+    }
+    @Test
+    public void MultipleBrackets () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[] [] []"));
+    }
+    @Test
+    public void FalseMultipleBrackets () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][ ][ ]["));
+    }
+    @Test
+    public void MixMatchBrackets () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+    @Test
+    public void MixedBrackets () {
+        assertFalse((BalancedBrackets.hasBalancedBrackets("[]]")));
+    }
+    @Test
+    public void  StringNoBracker () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("word"));
+    }
+    @Test
+    public void OneClosing () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void OneOpening () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void StringOpening () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("word]"));
+    }
+    @Test
+    public void StringClosing () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[word"));
+    }
+    @Test
+    public void MixStringBracket () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[wo][rd"));
+    }
 }
