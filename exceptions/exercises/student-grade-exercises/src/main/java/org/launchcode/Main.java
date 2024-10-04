6cd69e1d2756accc9f5a044b887e9e05bb8fa239
jayheid <jp.heidbrink@gmail.com>
2024-10-03 20:18:38 -0500

    modified exercise

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        Divide(4,0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +14,37 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        CheckFileExtension(studentFiles.get("Carl"));
+
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Can't divide by zero");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println(x / y);
+        }
+
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+
+        if(fileName.isBlank()){
+            try {throw new Exception("Filename Can't be Blank");
+            }catch (Exception e){
+                e.printStackTrace();
+                return -1;
+            }
+        }
+        else if (fileName.contains(".java")) {
+            return 1;
+        }
+        else{
+            return 0;
+        }
     }
 }
